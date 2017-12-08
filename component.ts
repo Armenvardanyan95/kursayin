9fd6e1eb7b282a246e781f38e1df0fb9505ab627
Armenvardanyan95 <armenvardanyan95@gmail.com>
2017-12-08 11:02:41 +0400

    FIx

--- component.ts
+++ component.ts
@@ -1,17 +1,37 @@
 declare const Rx: any;
 
 
-function proxify(o) {
+function getPropValue(propertyWithPrefixes: string, obj): any {
+    const propArray: string[] = propertyWithPrefixes.split('.');
+    return propArray.reduce((final, prop) => final = final[prop], obj)
+}
+
+function setPropValue(propertyWithPrefixes: string, target, value: any): void {
+    const propArray: string[] = propertyWithPrefixes.split('.');
+    if (propArray.length === 1) {
+        target[propArray[0]] = value;
+        return;
+    }
+    const upperMostNestedObject = getPropValue(propArray.slice(0, propArray.length - 1).join('.'), target);
+    upperMostNestedObject[propArray[propArray.length - 1]] = value;
+}
+
+function proxify(o, changeDetector) {
     function buildProxy(prefix, o) {
         return new Proxy(o, {
             set: function(target, propertyName, value) {
                 // same as before, but add prefix
-                console.log(`${propertyName} has been changed from ${target[propertyName]} to ${value}`);
+                console.log(`${prefix}${propertyName} has been changed from ${target[propertyName]} to ${value}`);
                 if (target[propertyName] !== value) {
-                    const changes = {[propertyName]: {previousValue: target[propertyName], currentValue: value}};
-                    if (target.__changeDetector) target.__changeDetector.next(changes);
+                    const changes = {[prefix + propertyName]: {previousValue: target[propertyName], currentValue: value}};
+                    if (changeDetector) changeDetector.next(changes);
                     if (target.onComponentChanges && typeof target.onComponentChanges === 'function') {
                         setTimeout(() => target.onComponentChanges(changes));
+                    }
+                    for (const prop in target) {
+                        if (typeof target[prop] === 'function' && target[prop].__watches === prefix + propertyName) {
+                            target[prop](target[propertyName], value);
+                        }
                     }
                 }
                 target[propertyName] = value;
@@ -74,7 +94,7 @@ function Component(options: {template: string, selector: string}) {
             __changeDetector = new Rx.BehaviorSubject(null);
             constructor(...args: any[]) {
                 super();
-                return proxify(this);
+                return proxify(this, this.__changeDetector);
             }
         }
     }
@@ -82,10 +102,8 @@ function Component(options: {template: string, selector: string}) {
 
 function Watch(property: string) {
     return function (target, method) {
-        // if (!(property in target)) {
-        //     throw ReferenceError(`Property ${property} does not exist`)
-        // }
-        console.log(target, method);
+        target[method].__watches = property;
+        console.log(target);
     }
 }
 
@@ -121,15 +139,31 @@ class Module {
                         const eventName: string = attrName.slice(3, attrName.length);
                         const methodNameEndIndex = node.attributes[attrName].lastIndexOf('(');
                         const methodName = node.attributes[attrName].slice(0, methodNameEndIndex);
-                        element.addEventListener(eventName, event => componentInstance[methodName]());
+                        Rx.Observable.fromEvent(element, eventName).subscribe(event => componentInstance[methodName]())
                     } else if (node.attributes.hasOwnProperty(attrName)) {
                         let finalAttrName: string;
+                        if (attrName === 'bind-model') {
+                            if (node.tagName !== 'input') {
+                                throw TypeError('You can only bind a model to an input element');
+                            }
+                            const bindingName: string = node.attributes[attrName];
+                            element.value = getPropValue(bindingName, componentInstance);
+                            componentInstance.__changeDetector
+                                .filter(changes => changes && changes[bindingName])
+                                .subscribe((changes) => element.value = changes[bindingName].currentValue);
+
+                            Rx.Observable
+                                .fromEvent(element, 'input')
+                                .subscribe(() => setPropValue(bindingName, componentInstance, element.value));
+                        }
                         if (attrName.startsWith('bind-')) {
                             finalAttrName = attrName.slice(5, attrName.length);
-                            const attr = node.attributes[attrName];
+                            const bindingName = node.attributes[attrName];
+                            const attrValue = getPropValue(bindingName, componentInstance);
+                            element[finalAttrName] = attrValue;
                             componentInstance.__changeDetector
-                                .filter(changes => changes && changes[attr])
-                                .subscribe(changes => element[finalAttrName] = changes[attr].currentValue);
+                                .filter(changes => changes && changes[bindingName])
+                                .subscribe(changes => element[finalAttrName] = changes[bindingName].currentValue);
                         } else {
                             element[attrName + (attrName === 'class' ? 'Name' : '')] = node.attributes[attrName];
                         }
@@ -145,7 +179,8 @@ class Module {
 @Component({
     template: `<div class="alert" style="background-color: aqua">
                 <button on-click="changeContent()">Click me!</button>
-                    Hello
+                    Hello <input type="text" bind-model="hello"/>
+                    <span bind-innerText="user.name">Hover me</span>
                     <nested></nested>
                     <nested></nested>
                     <nested></nested>
@@ -156,13 +191,12 @@ class Module {
     selector: 'greeter'
 })
 class Greeter {
-    property = "property";
-    hello: string;
+    property: string = "property";
+    hello: string = 'Private';
     isButtonInVisible: boolean = false;
     impedimenta: number[] = [];
-    constructor(m: string) {
-        this.hello = m;
-    }
+    user = {name: 'Armen'};
+    nestedObject = {name: 'Armen', anotherNested: {nested: 'privet'}};
 
     onComponentChanges(changes) {
         console.log(changes);
@@ -170,18 +204,31 @@ class Greeter {
 
     @Watch('hello')
     onHelloChange(previous: string, current: string) {
-        console.log(previous);
+        console.log(previous, 'jaaaan change ashxatum a');
     }
 
     changeContent(): void {
         this.isButtonInVisible = !this.isButtonInVisible;
+        this.user.name = 'Vardanyan';
     }
 }
 
 @Component({
-    template: `<p>Paragpaph is nested!</p>`,
+    template: `<p>Paragpaph is nested!
+                <a on-click="greet()">Hi!
+                </a>
+                <span bind-innerText="greetText"></span>
+                <input type="text" bind-model="greetText"/>
+            </p>`,
     selector: 'nested'
 })
-class ParagraphComponent {}
+class ParagraphComponent {
+    greetText = 'Hello, moto!'
+
+    greet() {
+        alert('Hello!')
+    }
+
+}
 
 const mdl = new Module([Greeter, ParagraphComponent], document.getElementById('root'));
